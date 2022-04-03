973718101115404df517ae35d7bf845e140d4bcd
dahye1013 <alrean@naver.com>
2022-04-04 01:55:41 +0900

    feat: kanban 설계 구현

--- src/App.ts
+++ src/App.ts
@@ -1,39 +1,58 @@
-import Kanban from "./Kanban";
-import TodoItem from "./TodoItem";
+import Kanban from './Kanban';
+import TodoItem from './TodoItem';
 
 interface EditKanbanOptions {
-    title: Kanban["title"];
+  title: Kanban['title'];
 }
 
 export default class App {
-    kanbans: Kanban[] = [];
-    archiveKanban: Kanban = new Kanban({title: "archive"});
+  kanbans: Kanban[] = [];
+  archiveKanban: Kanban = new Kanban({ title: 'archive' });
 
-    addKanban(kanban: Kanban) {
+  addKanban(kanban: Kanban) {
+    this.kanbans.push(kanban);
+  }
 
-    }
+  getKanbanById(id: Kanban['id']): Kanban {
+    return this.kanbans.find(kanban => kanban.id === id);
+  }
 
-    getKanbanById(id: Kanban['id']): Kanban {
+  getKanbanAll(): Kanban[] {
+    return this.kanbans;
+  }
 
-    }
+  editKanbanOrder(id: Kanban['id'], prevIndex: number, nextIndex: number) {
+    const editKanban = this.kanbans.splice(prevIndex, 1);
+    this.kanbans.splice(nextIndex, 0, ...editKanban);
+  }
 
-    getKanbanAll(): Kanban[] {
+  editKanbanTitle(id: Kanban['id'], options: EditKanbanOptions) {
+    const kanban = this.getKanbanById(id);
+    kanban.title = options.title;
+  }
 
-    }
+  moveTodoItem(
+    todoItemId: TodoItem['id'],
+    prevKanbanId: Kanban['id'],
+    nextKanbanId: Kanban['id']
+  ) {
+    const prevKanbanTodoList = this.getKanbanById(prevKanbanId).todoList;
+    const nextKanbanTodoList = this.getKanbanById(nextKanbanId).todoList;
+    const todoItem = prevKanbanTodoList.getItemById(todoItemId);
+    nextKanbanTodoList.addItem(todoItem);
+    prevKanbanTodoList.removeItemById(todoItemId);
+  }
 
-    editKanbanOrder(id: Kanban['id'], prevIndex: number, nextIndex: number) {
+  removeKanbanById(id: Kanban['id']) {
+    // 삭제된 칸반의 기존 할 일은 모두 Archive칸반으로 이동한다.
+    const removeKanban = this.kanbans.splice(id, 1)[0];
+    const archiveKanban = this.kanbans.find(
+      kanban => kanban.title === 'archive'
+    );
 
-    }
-
-    editKanbanTitle(id: Kanban['id'], options: EditKanbanOptions) {
-
-    }
-
-    moveTodoItem(todoItemId: TodoItem['id'], prevKanbanId: Kanban['id'], nextKanbanId: Kanban['id']) {
-
-    }
-
-    removeKanbanById(id: Kanban['id']) {
-        // 삭제된 칸반의 기존 할 일은 모두 Archive칸반으로 이동한다.
-    }
+    archiveKanban.todoList.list = [
+      ...archiveKanban.todoList.list,
+      ...removeKanban.todoList.list,
+    ];
+  }
 }
