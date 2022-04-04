47ba21db41183255d038061cf92279ae3ec9690e
jinsoobyeon <jinsoobyeon@gmail.com>
2022-04-04 15:34:05 +0900

    Feat: 콘솔 Todo 구현

--- src/index.js
+++ src/index.js
@@ -1,1 +1,71 @@
 "use strict";
+let todoId = 1;
+let kanbanId = 1;
+// 스토어
+const todoList = [];
+const kanbanList = [];
+// 카테고리
+const category = ["오늘 할 일", "내일 할 일", "다음 주 할 일"];
+// 할 일 추가
+const createTodo = (todoItem) => {
+    todoList.push(todoItem);
+    todoId++;
+    readTodoList();
+};
+//전체 할 일 가져오기
+const readTodoList = () => {
+    console.table(todoList);
+};
+// 특정 할 일 가져오기
+const readTodoDetail = (todoId) => {
+    console.table(todoList.find((todo) => todo.todoId === todoId));
+};
+// 할 일 수정
+const updateTodo = (todoId, content, finished, selectedCategory) => {
+    const todoItem = todoList.filter((todo) => todo.todoId === todoId);
+    todoItem[0].content = content;
+    todoItem[0].finished = finished;
+    todoItem[0].category = selectedCategory;
+    readTodoList();
+};
+// 특정 할 일의 특정 태그 수정
+const updateTodoTag = (todoId, selectedTag, tagValue) => {
+    var _a, _b;
+    const todoItem = todoList.filter((todo) => todo.todoId === todoId);
+    (_a = todoItem[0].tags) === null || _a === void 0 ? void 0 : _a.splice((_b = todoItem[0].tags) === null || _b === void 0 ? void 0 : _b.findIndex((tag) => tag === selectedTag), 1, tagValue);
+    readTodoList();
+};
+// 특정 할 일 제거
+const deleteTodoDetail = (todoId) => {
+    todoList.splice(todoList.findIndex((todo) => todo.todoId === todoId), 1);
+    readTodoList();
+};
+// 전체 할 일 제거
+const deleteTodoList = () => {
+    todoList.splice(0);
+    readTodoList();
+};
+// 특정 할 일의 특정 태그 제거
+const deleteTodoTag = (todoId, selectedTag) => {
+    var _a, _b;
+    const todoItem = todoList.filter((todo) => todo.todoId === todoId);
+    (_a = todoItem[0].tags) === null || _a === void 0 ? void 0 : _a.splice((_b = todoItem[0].tags) === null || _b === void 0 ? void 0 : _b.findIndex((tag) => tag === selectedTag), 1);
+    readTodoList();
+};
+// 특정 할 일의 모든 태그 제거
+const deleteTodoTagAll = (todoId) => {
+    var _a;
+    const todoItem = todoList.filter((todo) => todo.todoId === todoId);
+    (_a = todoItem[0].tags) === null || _a === void 0 ? void 0 : _a.splice(0);
+    readTodoList();
+};
+// 칸반 추가
+const createKanban = (kanban) => { };
+// 칸반 가져오기
+const readKanban = () => { };
+// 칸반 제목 수정
+const updateKanbanTitle = (kanbanId, title) => { };
+// 칸반 할 일 이동
+const updateKanbanTodo = (kanbanId, targetKanbanId, todoId) => { };
+// 칸반 제거
+const deleteKanban = (kanbanId) => { };
--- src/index.ts
+++ src/index.ts
@@ -27,13 +27,21 @@ const kanbanList: Kanban[] = [];
 const category: string[] = ["오늘 할 일", "내일 할 일", "다음 주 할 일"];
 
 // 할 일 추가
-const createTodo = (todoItem: TodoItem): void => {};
+const createTodo = (todoItem: TodoItem): void => {
+  todoList.push(todoItem);
+  todoId++;
+  readTodoList();
+};
 
 //전체 할 일 가져오기
-const readTodoList = (): void => {};
+const readTodoList = (): void => {
+  console.table(todoList);
+};
 
 // 특정 할 일 가져오기
-const readTodoDetail = (todoId: number): void => {};
+const readTodoDetail = (todoId: number): void => {
+  console.table(todoList.find((todo: TodoItem) => todo.todoId === todoId));
+};
 
 // 할 일 수정
 const updateTodo = (
@@ -41,26 +49,68 @@ const updateTodo = (
   content: string,
   finished: boolean,
   selectedCategory: "오늘 할 일" | "내일 할 일" | "다음 주 할 일"
-): void => {};
+): void => {
+  const todoItem: TodoItem[] = todoList.filter(
+    (todo: TodoItem) => todo.todoId === todoId
+  );
+  todoItem[0].content = content;
+  todoItem[0].finished = finished;
+  todoItem[0].category = selectedCategory;
+  readTodoList();
+};
 
 // 특정 할 일의 특정 태그 수정
 const updateTodoTag = (
   todoId: number,
   selectedTag: Tag,
   tagValue: Tag
-): void => {};
+): void => {
+  const todoItem: TodoItem[] = todoList.filter(
+    (todo: TodoItem) => todo.todoId === todoId
+  );
+  todoItem[0].tags?.splice(
+    todoItem[0].tags?.findIndex((tag: Tag) => tag === selectedTag),
+    1,
+    tagValue
+  );
+  readTodoList();
+};
 
 // 특정 할 일 제거
-const deleteTodoDetail = (todoId: number): void => {};
+const deleteTodoDetail = (todoId: number): void => {
+  todoList.splice(
+    todoList.findIndex((todo: TodoItem) => todo.todoId === todoId),
+    1
+  );
+  readTodoList();
+};
 
 // 전체 할 일 제거
-const deleteTodoList = (): void => {};
+const deleteTodoList = (): void => {
+  todoList.splice(0);
+  readTodoList();
+};
 
 // 특정 할 일의 특정 태그 제거
-const deleteTodoTag = (todoId: number, selectedTag: Tag): void => {};
+const deleteTodoTag = (todoId: number, selectedTag: Tag): void => {
+  const todoItem: TodoItem[] = todoList.filter(
+    (todo: TodoItem) => todo.todoId === todoId
+  );
+  todoItem[0].tags?.splice(
+    todoItem[0].tags?.findIndex((tag: Tag) => tag === selectedTag),
+    1
+  );
+  readTodoList();
+};
 
 // 특정 할 일의 모든 태그 제거
-const deleteTodoTagAll = (todoId: number): void => {};
+const deleteTodoTagAll = (todoId: number): void => {
+  const todoItem: TodoItem[] = todoList.filter(
+    (todo: TodoItem) => todo.todoId === todoId
+  );
+  todoItem[0].tags?.splice(0);
+  readTodoList();
+};
 
 // 칸반 추가
 const createKanban = (kanban: Kanban): void => {};
